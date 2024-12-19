342a8f87114157c40c6226097a4dc74da30bdacc
ChaiTope <gksmdk5094@gmail.com>
2024-12-19 17:59:34 +0900

    Add PostView, Comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/style.css';
 import AnimatedCursor from "react-animated-cursor"
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import Login from './pages/Login';
+import Login from './pages/Login'
 
 import Layout from './pages/Layout'
 import Main from './pages/Main'
@@ -14,52 +14,54 @@ import Contact from './pages/Contact';
 import Git from './pages/Git';
 import View from './pages/View';
 
-import { AuthProvider, AuthContext } from './context/AuthContext';
 
-/** 관리자 **/
+import { AuthProvider, AuthContext } from './context/AuthContext'
+
+/** 관리자 */
 import PostWrite from './pages/admin/PostWrite';
 
-/* 인증 상태에 따라 라우팅 제어 */
-const PrivateRoute = ({children, adminOnly = false}) => {
-  const { isAuthenticated, user} = useContext(AuthContext);
+/** 인증 상태에 따라 라우팅 제어 */
+const PrivateRoute = ({ children, adminOnly = false })=> {
+   const { isAuthenticated, user} = useContext( AuthContext );
 
-  if(!isAuthenticated) {  //로그인 안되어 있으면 로그인 페이지
-      return <Navigate to="/login" replace />;
-  }
+   if(!isAuthenticated) {  //로그인 안되어 있으면 로그인 페이지
+       return <Navigate to="/login" replace />;
+   }
 
-  if(adminOnly && user.role === 'admin') {
-    return <Navigate to="/" replace />;
-  }
+   if(adminOnly && user.role === 'admin') {
+      return <Navigate to="/" replace />;
+   }
 
-  return children;
+   return children;
 }
+
 
 const App = () => {
   return (
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-    <AuthProvider>
-      <AnimatedCursor />
-      <Router>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<Main />} />
-                  <Route path="post" element={<Post />} />
-                  <Route path="about" element={<About />} />
-                  <Route path="git" element={<Git />}/>
-                  <Route path="contact" element={<Contact />} />
-                  <Route path="view/:post" element={<View />} />
-                  <Route path="login" element={<Login />} />
-                  <Route path="postwrite" element={
-                    // <PrivateRoute adminOnly={true}>
-                      <PostWrite />
-                    // </PrivateRoute>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <AnimatedCursor />
+        <Router>
+              <Routes>
+                  <Route path="/" element={<Layout />}>
+                    <Route index element={<Main />} />
+                    <Route path="post" element={<Post />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="git" element={<Git />}/>
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="view/:post" element={<View />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="postwrite" element={
+                      <PrivateRoute adminOnly={true}>
+                        <PostWrite />
+                      </PrivateRoute> 
                     } />
-                </Route>
-            </Routes>
+                  </Route>
+              </Routes>
 
-      </Router>
-    </AuthProvider>  
-  </GoogleOAuthProvider>
+        </Router>
+      </AuthProvider>
+    </GoogleOAuthProvider>    
   )
 }
 
--- frontend/src/pages/CommentWrtie.jsx
+++ frontend/src/pages/CommentWrtie.jsx
@@ -1,19 +1,60 @@
-import React, { useContext, useState }from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import Wysiwyg2 from '../components/Write/Wysiwyg2'
+import { Container, Row, Col } from 'react-bootstrap'
 
-const CommentWrtie = () => {
+import axios from 'axios'
+
+const CommentWrite = ({ postId, post }) => {
   const { user } = useContext(AuthContext);
-  const { comment, setComment } = useState('');
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const commentData = {
+       postId : postId,
+       username: user.name,
+       useremail: user.email,
+       social: user.social,
+       comment
+    }
+    console.log(commentData);
+    try{
+         await axios.post('http://localhost:8080/api/posts/comment', commentData, {
+            headers: {
+              'Content-Type' : 'application/json'
+            }
+         });
+         alert("정상적으로 저장 되었습니다.");
+         
+    }catch(error){
+      console.error('에러가 발생했습니다.', error);
+      alert('댓글을 쓰는 도중 에러가 발생했습니다.');
+    }
+  }
 
   return (
-    <div>
-        <form>
-            <input type="text" value={user.name} readOnly />
-            <br />
-            <textarea name="comment" value={comment}></textarea>
-        </form>
-    </div>
+    <Container>
+      <form onSubmit={handleSubmit}>
+      <Row className="mt-5">
+        <Col md="2" className="text-end">이름</Col>
+        <Col md="10">
+           {user.name}
+        </Col>    
+
+        <Col md='2' className="mt-3 text-end">내용</Col>
+        <Col md="10" className="mt-3 coment-wysiwyg">
+           <Wysiwyg2 content={comment}
+                    setContent={setComment}
+           />         
+        </Col> 
+      </Row>
+      <div className="text-center mt-5">
+          <button type="submit" className="btn btn-primary">저장</button>
+      </div>
+      </form>
+    </Container>    
   )
 }
 
-export default CommentWrtie
+export default CommentWrite
